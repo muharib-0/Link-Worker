3709d2ba81393d0e3bff954bd745e8da55fe9599
muharib-0 <mohammedfayazhussain13@gmail.com>
2025-12-24 23:56:08 +0530

    Feat: 1.included the login feature

--- signup/views.py
+++ signup/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serialization import SignupSerializer
 from .models import userprofile
+from django.contrib.auth.hashers import check_password
 # Create your views here.
 class signupview(APIView):
     def post(self, request):
-        # user=userprofile.objects.all()
         serializer = SignupSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -17,4 +17,17 @@ class signupview(APIView):
         serializer = SignupSerializer(users, many=True)
         return Response(serializer.data)    
     
+class loginview(APIView):
+    def post(self,request):
+        email=request.data.get('email')
+        password=request.data.get('password')
+        try:
+            user=userprofile.objects.get(email=email)
+            if check_password(password, user.password):
+                return Response({"message": f"Login successful! Welcome back, {user.username}."},status=200)
+            else:
+                return Response({"message":"invalid credentials"},status=401)
+        except userprofile.DoesNotExist:
+            return Response({"message":"user not found"},status=404)
+
     
