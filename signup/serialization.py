f14e3908787290151c18a1bc49cc303ec1a7dfee
muharib-0 <mohammedfayazhussain13@gmail.com>
2025-12-24 23:55:27 +0530

    feat:changed the serializer to hide the password after the post action 2.hashing of password using set_password function

--- signup/serialization.py
+++ signup/serialization.py
@@ -6,3 +6,17 @@ class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = userprofile
         fields = ['id', 'username', 'email', 'password', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
+    def create(self, validated_data):
+    
+        password = validated_data.pop('password')
+        user = userprofile(**validated_data)
+        user.set_password(password)
+        user.save() 
+        return user
+
+
+class loginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = userprofile
+        fields = ['email', 'password']        
